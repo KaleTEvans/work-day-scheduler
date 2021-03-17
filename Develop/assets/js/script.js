706706df334ae881779de0155f054ba3454756a6
KaleTEvans <kaletevans@gmail.com>
2021-03-17 12:11:46 -0500

    added initial html layout

---
+++ Develop/assets/js/script.js
@@ -0,0 +1,20 @@
+/*********************** Work Day Scheduler ******************** */
+/* Create HTML elements for a schedule containing 9 one hour blocks 
+    for each hour of the work day. The blocks should contain the time,
+    a space for a form to enter the task description, and a button to 
+    save the entry. The past hours should be displayed as gray, the 
+    current hour displayed as red, and future hours displayed as green.
+
+    FUNCTIONS
+    NOTE: No create task function should be needed, as the task blocks
+        have already been designated
+    1. Add a function to edit a time block to add a task
+    2. Add a function to listen for clicks in the time block area to edit tasks
+    3. Add a function to listen for clicks in the save column to save tasks
+    4. Add a save tasks function to save to local storage
+    5. Add a load tasks function to create task persistence
+    6. Add a function to change time block colors based on current time
+    7. Add a function to measure time and send updates to function 7
+    8. Add a function to update current day at the top of the page */
+
+    
