a725f8359c43b2e60b51f5344f9615c4e188a10c
KaleTEvans <kaletevans@gmail.com>
2021-03-20 11:10:22 -0500

    edited function to load tasks upon refresh

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -5,18 +5,14 @@
     save the entry. The past hours should be displayed as gray, the 
     current hour displayed as red, and future hours displayed as green.
 
-    FUNCTIONS
-    NOTE: No create task function should be needed, as the task blocks
-        have already been designated
-    1. Add a function to edit a time block to add a task
-    2. Add a function to listen for clicks in the time block area to edit tasks
-    3. Add a function to listen for clicks in the save column to save tasks
-    4. Add a save tasks function to save to local storage
-    5. Add a load tasks function to create task persistence
-    6. Add a function to change time block colors based on current time
-    7. Add a function to measure time and send updates to function 7
-    8. Add a function to update current day at the top of the page */
+    1. When a textarea is clicked, enter new text for task data
+    2. When the save button is clicked, save the text in the text area
+        along with the time block id into an object
+    3. When the page is refreshed, the load task function should loop 
+        through each object, match the saved time with the time block id
+        and fill in the text area with the saved text
 
+*/
 var tasks = [];
 
 // save button was clicked
@@ -28,9 +24,10 @@ $(".saveBtn").click(function() {
 
     // store the values into the tasks variable
     if (taskText) {
+
         tasks.push({
             text: taskText,
-            date: taskTime
+            time: taskTime
         });
 
         saveTasks();
@@ -38,7 +35,37 @@ $(".saveBtn").click(function() {
 });
 
 // load tasks function
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    $('textarea').each(function(index, value) {
+        var time = (this.id);
+        console.log(time);
+        tasks.forEach(function(item) {
+            if (item.time === time) {
+                console.log("true");
+                $("#" + time).text(item.text);
+            }
+            else {
+                return;
+            }
+        });
+    });
+
+};
+
+// function to track hours of the day
+var timeSlots = function() {
+    // get the time data
+    var currentTime = moment().hour();
+    //
+    $(".description").each(function() {
+        var hourSlot = parseInt($(this).attr("id").split(hour)[1]);
+    })
+}
 
 var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+loadTasks();
